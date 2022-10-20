4465a8513c2bf8ed26eb0acc4e29bdce590c7cc5
smrTofighi <mrtofxn@gmail.com>
2022-10-20 16:34:41 -0300

    create and coplete login screen

---
+++ lib/constant/strings.dart
@@ -0,0 +1,17 @@
+class Strings {
+  static const String goodAfternonText = 'عصرت بخیر';
+  static const String bestText = 'برترین ها';
+  static const String nearFromYou = 'نزدیک ترین ها';
+  static const String seeAllText = 'همه رو ببین';
+  static const String searchHintText = 'هرجایی رو میخوای جستجو کن ):';
+  static const String totalSpaceForCars = 'فضای موجود';
+  static const String loginText = 'ورود';
+  static const String youDontHaveAnyAccountText = 'حساب کاربری نداری؟';
+  static const String createOneText = 'یکی بساز';
+  static const String helloAgainText = 'سلام دوباره!';
+  static const String recoveryPasswordText = 'بازیابی رمز عبور';
+  static const String textForUserFromLoginScreenText = '''
+خوش اومدید
+دلمون برات تنگ شده بود
+''';
+}
--- lib/views/screens/login_screen.dart
+++ lib/views/screens/login_screen.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
-import 'package:park_app/views/screens/main_screen.dart';
+import 'package:park_app/constant/colors.dart';
+import 'package:park_app/constant/strings.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -12,12 +10,143 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Center(
-          child: ElevatedButton(
-            onPressed: () {
-              Get.off(const MainScreen());
-            },
-            child: const Text('صفحه اصلی'),
+        backgroundColor: Colors.grey[300],
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(
+              height: 40.0,
+            ),
+            helloAgainText(),
+            const SizedBox(
+              height: 10.0,
+            ),
+            messageForUserText(),
+            const SizedBox(
+              height: 16.0,
+            ),
+            emailTextField(),
+            const SizedBox(
+              height: 16.0,
+            ),
+            passwordTextField(),
+            recoveryPasswordTextButton(),
+            loginButton(),
+            const Spacer(),
+            createAccountRowButton(),
+            const SizedBox(
+              height: 60,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Row createAccountRowButton() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          Strings.youDontHaveAnyAccountText,
+          style: TextStyle(fontSize: 13, color: Colors.grey[700]),
+        ),
+        TextButton(
+          onPressed: () {},
+          style: ButtonStyle(
+            textStyle: MaterialStateProperty.all(
+              const TextStyle(fontSize: 13),
+            ),
+            foregroundColor: MaterialStateProperty.all(SolidColors.blueColor),
+          ),
+          child: const Text(Strings.createOneText),
+        )
+      ],
+    );
+  }
+
+  Padding loginButton() {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(32, 8, 32, 36),
+      child: SizedBox(
+        width: Get.width,
+        height: 50,
+        child: ElevatedButton(
+          onPressed: () {},
+          style: ButtonStyle(
+            backgroundColor:
+                MaterialStateProperty.all(SolidColors.backGroundColor),
+          ),
+          child: const Text(Strings.loginText),
+        ),
+      ),
+    );
+  }
+
+  Padding recoveryPasswordTextButton() {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(32, 16, 0, 16),
+      child: Align(
+        alignment: Alignment.centerLeft,
+        child: TextButton(
+          onPressed: () {},
+          style: ButtonStyle(
+            textStyle: MaterialStateProperty.all(
+              const TextStyle(fontSize: 13),
+            ),
+            foregroundColor: MaterialStateProperty.all(Colors.grey[700]),
+          ),
+          child: const Text(Strings.recoveryPasswordText),
+        ),
+      ),
+    );
+  }
+
+  Padding passwordTextField() {
+    return const Padding(
+      padding: EdgeInsets.fromLTRB(32, 0, 32, 0),
+      child: TextField(
+        decoration: InputDecoration(
+          hintText: 'رمز عبور',
+          hintStyle: TextStyle(fontSize: 14),
+        ),
+      ),
+    );
+  }
+
+  Padding emailTextField() {
+    return const Padding(
+      padding: EdgeInsets.fromLTRB(32, 0, 32, 8),
+      child: TextField(
+        decoration: InputDecoration(
+          hintStyle: TextStyle(fontSize: 14),
+          hintText: 'ایمیل',
+        ),
+      ),
+    );
+  }
+
+  Text messageForUserText() {
+    return Text(
+      Strings.textForUserFromLoginScreenText,
+      textAlign: TextAlign.center,
+      style: TextStyle(
+        fontSize: 18,
+        color: Colors.grey[700],
+      ),
+    );
+  }
+
+  SizedBox helloAgainText() {
+    return SizedBox(
+      width: Get.width,
+      child: const Align(
+        alignment: Alignment.center,
+        child: Text(
+          Strings.helloAgainText,
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 25,
           ),
         ),
       ),
